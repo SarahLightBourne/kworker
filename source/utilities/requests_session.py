78425b69383e15ff948494c7977e74674e2f1de1
SarahLightBourne <SaraLightBourne@gmail.com>
2022-06-01 07:06:43 +0300

    Fake Headers

--- source/utilities/requests_session.py
+++ source/utilities/requests_session.py
@@ -1,4 +1,5 @@
 from httpx import AsyncClient
+from fake_headers import Headers
 
 
 class RequestsSession:
@@ -7,7 +8,7 @@ class RequestsSession:
     self.requests_session: AsyncClient = None
 
   async def get_requests_session(self) -> AsyncClient:
-    return self.requests_session or AsyncClient(timeout=1000)
+    return self.requests_session or AsyncClient(timeout=1000, headers=Headers().generate())
 
   async def close_requests_session(self) -> None:
     if self.requests_session:
